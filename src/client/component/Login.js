0274826237da8a87f0f87cd551f0dc472835e58d
Wenbin Liu <liuwenbin.lwb@gmail.com>
2018-11-13 23:42:22 +0800

    update for login UI

--- src/client/component/Login.js
+++ src/client/component/Login.js
@@ -4,36 +4,64 @@ import './Login.scss';
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        const self = this;
-        self.state = {
+        this.state = {
             loginType: null,
+            user: '',
+            password: '',
         }
         this.showLoginInput = this.showLoginInput.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.onUserChange = this.onUserChange.bind(this);
+        this.login = this.login.bind(this);
     }
 
     showLoginInput(loginType) {
+        this.reset();
         this.setState({ loginType })
+    }
+
+    onUserChange(e) {
+        this.setState({ user: e.target.value })
+    }
+
+    onPasswordChange(e) {
+        this.setState({ password: e.target.value })
+    }
+
+    login() {
+        const { user, password } = this.state;
+        console.log('userName', user);
+        console.log('password', password);
+        this.reset();
+    }
+
+    reset() {
+        this.setState({ loginType: null, user: '', password: '' })
     }
 
     render() {
         const self = this;
-        const { loginType } = self.state;
+        const { loginType, user, password } = self.state;
         return (
             <div className="login-container">
-                <div className="button" onClick={() => self.showLoginInput('phone')}>用手机号登录</div>
-                <div className="button" onClick={() => self.showLoginInput('email')}> 使用邮箱登录</div>
-                {loginType === "phone" && (
-                    <div>
-                        <input type="text"></input>
-                        <input type="password"></input>
+                {!loginType && <div className="button" onClick={() => self.showLoginInput('phone')}>用手机号登录</div>}
+                {!loginType && <div className="button" onClick={() => self.showLoginInput('email')}>使用邮箱登录</div>}
+                {loginType &&
+                    <div className="input-container">
+                        <div className="login-user-info-container">
+                            <span className="label">{loginType === "phone" ? "Phone" : "Email"}</span>
+                            <input type="text" className="input-box" onChange={self.onUserChange} value={user}></input>
+                        </div>
+                        <div className="login-user-info-container">
+                            <span className="label">Password</span>
+                            <input type="password" name="Password" className="input-box" onChange={self.onPasswordChange} value={password}></input>
+                        </div>
+                        <div className="button-container">
+                            <div className="button" onClick={self.login}>登录</div>
+                            <div className="button" onClick={() => self.showLoginInput(null)}>返回</div>
+                        </div>
                     </div>
-                )}
-                {loginType === "email" && (
-                    <div>
-                        <input type="text"></input>
-                        <input type="password"></input>
-                    </div>
-                )}
+                }
             </div>
         )
     }
